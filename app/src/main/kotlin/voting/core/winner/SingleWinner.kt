40f3eac47145a317f5e7b87e402feca0e11e809f
Matthew Evans <mcaei@fastmail.com>
2025-02-23 22:42:31 -0700

    Just do the elections on page load.
    I've learned it's usually easiest to just hx-boost the whole page rather than ship fragments over the wire. This means we just send the whole page back if there's an update and replace only the relevant parts.

--- app/src/main/kotlin/voting/core/winner/SingleWinner.kt
+++ app/src/main/kotlin/voting/core/winner/SingleWinner.kt
@@ -1,5 +1,10 @@
 package voting.core.winner
 
 import voting.core.Candidate
+import voting.core.District
 
-class SingleWinner(val candidate: Candidate, val votes: Int, val totalVotes: Int) : Winner
+class SingleWinner(val candidate: Candidate, val votes: Int, val totalVotes: Int) : Winner {
+    fun votePercent(): Float {
+        return (this.votes.toFloat() / this.totalVotes.toFloat()) * 100;
+    }
+}
--- app/src/main/kotlin/voting/web/plugins/Templating.kt
+++ app/src/main/kotlin/voting/web/plugins/Templating.kt
@@ -17,6 +17,42 @@ import voting.core.system.FirstPastThePost
 import voting.core.system.InstantRunoff
 import voting.core.vote.RankedCandidateVote
 import voting.core.vote.SingleCandidateVote
+import voting.core.winner.SingleWinner
+
+fun runFptp(): Pair<SingleWinner, District> {
+    val molly = Candidate("Molly", Party("Dog Party"))
+    val bean = Candidate("Bean", Party("Cat Party"))
+    val district = District("Hexadecimal City", setOf(molly, bean))
+    val system = FirstPastThePost()
+    val election = Election(district, system)
+    val candidates = setOf(molly, bean)
+    election.castVote(SingleCandidateVote(candidates.first()))
+    election.castVote(SingleCandidateVote(candidates.first()))
+    election.castVote(SingleCandidateVote(candidates.first()))
+    election.castVote(SingleCandidateVote(candidates.last()))
+    val result = election.result()
+
+    return Pair(result, district);
+}
+
+fun runInstantRunoff(): Pair<SingleWinner, District> {
+    val willa = Candidate("Willa", Party("Dog Party"))
+    val aristotle = Candidate("Aristotle", Party("Cat Party"))
+    val district = District("Octal Town", setOf(willa, aristotle))
+    val candidates = setOf(aristotle, willa)
+    val system = InstantRunoff()
+    val election = Election(district, system)
+    election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
+    election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
+    election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
+    election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
+    election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
+    election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
+    election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
+    val result = election.result()
+
+    return Pair(result, district)
+}
 
 fun Application.configureTemplating() {
     install(Mustache) {
@@ -24,65 +60,31 @@ fun Application.configureTemplating() {
     }
     routing {
         get("/") {
-            call.respond(MustacheContent("index.html", mapOf("name" to "Matthew")))
-        }
-        get("/templates/fptp") {
-            val molly = Candidate("Molly", Party("Dog Party"))
-            val bean = Candidate("Bean", Party("Cat Party"))
-            val district = District("Hexadecimal City", setOf(molly, bean))
-            val system = FirstPastThePost()
-            val election = Election(district, system)
-            val candidates = setOf(molly, bean)
-            election.castVote(SingleCandidateVote(candidates.first()))
-            election.castVote(SingleCandidateVote(candidates.first()))
-            election.castVote(SingleCandidateVote(candidates.first()))
-            election.castVote(SingleCandidateVote(candidates.last()))
-            val result = election.result()
-            val votePercent = (result.votes.toFloat() / result.totalVotes.toFloat()) * 100
+            val fptp = runFptp();
+            val ir = runInstantRunoff();
 
             call.respond(
                 MustacheContent(
-                    "single-winner.html",
-                    mapOf(
-                        "system" to "First Past the Post",
-                        "district" to district.name,
-                        "candidate" to result.candidate.name,
-                        "party" to result.candidate.party.name,
-                        "votes" to result.votes,
-                        "votePercent" to votePercent,
-                    ),
-                ),
-            )
-        }
-        get("/templates/runoff") {
-            val willa = Candidate("Willa", Party("Dog Party"))
-            val aristotle = Candidate("Aristotle", Party("Cat Party"))
-            val district = District("Octal Town", setOf(willa, aristotle))
-            val candidates = setOf(aristotle, willa)
-            val system = InstantRunoff()
-            val election = Election(district, system)
-            election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
-            election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
-            election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
-            election.castVote(RankedCandidateVote(setOf(candidates.first(), candidates.last())))
-            election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
-            election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
-            election.castVote(RankedCandidateVote(setOf(candidates.last(), candidates.first())))
-            val result = election.result()
-            val votePercent = String.format("%.1f", (result.votes.toFloat() / result.totalVotes.toFloat()) * 100)
-
-            call.respond(
-                MustacheContent(
-                    "single-winner.html",
-                    mapOf(
-                        "system" to "Instant Runoff",
-                        "district" to district.name,
-                        "candidate" to result.candidate.name,
-                        "party" to result.candidate.party.name,
-                        "votes" to result.votes,
-                        "votePercent" to votePercent,
-                    ),
-                ),
+                    "index.html", mapOf(
+                        "results" to listOf(
+                            mapOf(
+                                "system" to "First Past the Post",
+                                "district" to fptp.second.name,
+                                "candidate" to fptp.first.candidate.name,
+                                "party" to fptp.first.candidate.party.name,
+                                "votes" to fptp.first.votes,
+                                "votePercent" to fptp.first.votePercent(),
+                            ), mapOf(
+                                "system" to "Instant Runoff",
+                                "district" to ir.second.name,
+                                "candidate" to ir.first.candidate.name,
+                                "party" to ir.first.candidate.party.name,
+                                "votes" to ir.first.votes,
+                                "votePercent" to ir.first.votePercent(),
+                            )
+                        )
+                    )
+                )
             )
         }
     }
